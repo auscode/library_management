6a79c44dd9edd76ee463c0a3706dfcae18bc7b9d
auscode <harshittext@gmail.com>
2024-07-23 11:07:17 +0530

    HELLO RENDER

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,11 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return "Hello, Render!"
+
     app.config.from_object('config.Config')
 
     db.init_app(app)
