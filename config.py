945a1e6ec97ead7eb6b5df5e69b0fb40953aed2b
auscode <harshittext@gmail.com>
2024-07-23 14:46:25 +0530

    config mod

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
+import os
+
 class Config:
-    SECRET_KEY = 'key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret-key'
 
-class DevelopmentConfig(Config):
-    DEBUG = True
 
-class ProductionConfig(Config):
-    DEBUG = False
+# class Config:
+#     SECRET_KEY = 'key'
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# class DevelopmentConfig(Config):
+#     DEBUG = True
+
+# class ProductionConfig(Config):
+#     DEBUG = False
